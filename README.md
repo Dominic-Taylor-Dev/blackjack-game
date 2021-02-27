ef1c088564279b5537bab4ac28469d429131a32f
Dominic-Taylor-Dev <email.dom.taylor@gmail.com>
2021-02-27 00:03:07 +0000

    Update to make the game against a dealer

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <h1> Blackjack Game </h1>
 
 <h2> How to Use </h2>
-This is a very simple, one-file game, intended to be played from the command line. It already contains the "run_game" instruction (which is a loop method) inside the code, so simply launching the game file from the command line should kick you off. The steps to get it running are:
+This is a very simple, one-file game - less than 100 lines all in - created in Ruby and intended to be played from the command line/terminal. It already contains the "run_game" instruction (which is a loop method) inside the code, so simply launching the game file ("blackjack.rb") from the command line should kick you off. The steps to get it running are:
   
   1. Download the blackjack.rb file to your computer.
   2. Launch terminal.
@@ -12,14 +12,15 @@ This is a very simple, one-file game, intended to be played from the command lin
 
 <h2> How to Play </h2>
 
-You only have two options: hit or stick. Follow the onscreen instructions - the game will end when you either choose to stick at your current score, or when you go bust (i.e. you amass over 21 points).
+You only have two options: 'hit' or 'stick'. Follow the onscreen instructions - the game will end when you either choose to stick at your current score, or when you go bust (i.e. if you have over 21 points).
 
-Rules are the same as normal Blackjack except that aces have a fixed value of 11, rather than being optionally 1 or 11. This was for coding simplicity more than anything else...!
+Rules are the same as normal Blackjack except that aces have a fixed value of 11, rather than being optionally 1 or 11. This was for coding simplicity more than anything else although I may revisit that at some point. It'd just be a bit of extra logic to say broadly "aces are 11 by default but if there are aces in the hand, and random card push to player hand would result in bust, recalculate hand value with ace as 1".
 
 <h2> How the code is structured </h2>
 
-There are four methods:
+There are five methods:
 1. random_card: This picks a value at random from a list of card values.
 2. move: This asks the player for a move.  If they enter hit or stick, it returns the move. Otherwise,it keeps asking.
 3. score: Translates the string of the card into a value
-4. run_game: the loop which pulls in all the other methods and sets win/loss/break conditions.
+4. dealer_score_calc: I kept it simple, but basically this is a very rough approximation of likely dealer scores i.e. 2/7 chance of bust and 1/7 chance of any score from 17-21.
+5. run_game: the loop which pulls in all the other methods and sets win/loss/break conditions.
--- blackjack.rb
+++ blackjack.rb
@@ -1,87 +1,37 @@
-# To complete this challenge, focus on one method at a time.
-#
-# Write a program that lets a user player a solo game of
-# Blackjack. The program should:
-# * Ask the player if they want to "hit" or "stick".
-#   * If the player hits, add a card to their hand.
-#   * If the player sticks, end the game.
-# * Keep asking the player if they want to "hit" or "stick" until they
-#   say "stick".
-# * Each time the player hits, calculate the score for the player's
-#   hand and `puts` `Score so far: ` and the score.
-#   * e.g. `Score so far: 23`
-#   * A score is calculated by adding up the values of each of the
-#     cards in the player's hand.
-#   * Value for each card:
-#     * "two": 2
-#     * "three": 3
-#     * "four": 4
-#     * "five": 5
-#     * "six": 6
-#     * "seven": 7
-#     * "eight": 8
-#     * "nine": 9
-#     * "ten": 10
-#     * "jack": 10
-#     * "queen": 10
-#     * "king": 10
-#     * "ace": 11 (This is slightly different from real Blackjack.)
-# * When the game is over, `puts` the outcome of the game.
-#   * If the player's score is `<= 21`, `puts` `You scored: ` and the
-#     final score
-#     * e.g. `You scored: 20`
-#   * If the player's score is `> 21`, `puts` `You busted with: ` and
-#     the final score.
-#     * e.g. `You busted with: 25`
-
-# * As part of your solution, there should be four specific methods:
-#   * `random_card`: This has already been written for you.  You don't
-#     need to change it.
-#   * `move`: Asks the player for a move.  If they enter `hit` or
-#     `stick`, it returns the move.  If they enter something else, it
-#     keeps asking them until they enter `hit` or `stick`.
-#   * `score`: Takes an array of cards and returns the score for the
-#     hand as an integer.
-#   * `run_game`: uses the other methods to run a game of Blackjack.
-#
-# * Note: When you run the automated tests, make sure to remove from
-#   the top level of the file any calls to `run_test` or other
-#   methods.
-#
-# * Note: To stop the game when the user sticks, don't use `exit` to
-#   quit the program because this will break the automated tests.  To
-#   exit a while loop early, use the `break` keyword.
-
-# You don't need to change this method!
-
 CARD_VALUES = {
-  "two" => 2,
-  "three" => 3,
-  "four" => 4,
-  "five" => 5,
-  "six" => 6,
-  "seven" => 7,
-  "eight" => 8,
-  "nine" => 9,
-  "ten" => 10,
-  "jack" => 10,
-  "queen" => 10,
-  "king" => 10,
-  "ace" => 11, 
+  "A" => 1,
+  "2" => 2,
+  "3" => 3,
+  "4" => 4,
+  "5" => 5,
+  "6" => 6,
+  "7" => 7,
+  "8" => 8,
+  "9" => 9,
+  "10" => 10,
+  "J" => 10,
+  "Q" => 10,
+  "K" => 10,
 }
 
 def random_card
-  cards = ["two", "three", "four", "five", "six", "seven",
-           "eight", "nine", "ten",
-           "jack", "queen", "king", "ace"]
+  values = ["2", "3", "4", "5", "6", "7",
+           "8", "9", "10", "J", "Q", "K", "A"]
 
-  cards[rand(13)]
+  suits = ["♠ ", "♥ ", "♦ ", "♣ "]
+
+# "|" are just spacers for clearer visualisation when it puts later
+  random_card = ["|"]
+  random_card.push(values[rand(13)])
+  random_card.push(suits[rand(4)])
+  random_card.push("| ")
+  return random_card
 end
 
 def move
   loop do
     puts "Do you want to 'hit' or 'stick'?"
-    move = gets.chomp
+    move = gets.chomp.downcase
     if (move == 'hit' || move == 'stick')
       return move
     end
@@ -90,26 +40,53 @@ end
 
 def score(card_arr)
   card_values = []
-  card_arr.each { |i| card_values.push(CARD_VALUES[i]) }
+  card_arr.each { |spacer1, value, suit, spacer2| card_values.push(CARD_VALUES[value]) }
   return card_values.sum
 end
 
+def dealer_score_calc
+  possible_scores = [0, 0, 17, 18, 19, 20, 21]
+  return possible_scores[rand(7)]
+end
+
+####################
+##MAIN GAME#########
+####################
+
 def run_game
+  
+  dealer_score = dealer_score_calc
   
   player_score = 0
   player_hand = []
-  
+       
   loop do
     if player_score > 21
-      puts "You busted with: #{player_score}"
+      puts "You busted with: "+ player_score.to_s
+      puts "Your final hand: "+ player_hand.join("")
+      puts "Dealer scored: "+ dealer_score.to_s
+      puts "DEALER WINS!"
       break
     elsif move == "stick" 
-      puts "You scored: #{player_score}"
+      puts "----------------"
+      puts "You scored: "+ player_score.to_s
+      puts "Your final hand: " + player_hand.join("")
+      puts "Dealer scored: "+ dealer_score.to_s
+        if dealer_score > player_score
+          puts "DEALER WINS!"
+        elsif dealer_score < player_score
+          puts "YOU WIN!"
+        else
+          puts "IT'S A TIE"
+        end
       break
     else
       player_hand.push(random_card)
       player_score = score(player_hand)
-      puts "Score so far: #{player_score}"
+      puts "----------------"
+      puts "Score so far: "+ player_score.to_s
+      puts "Hand so far: " + player_hand.join("")
+      puts "----------------"
     end
   end
 end
